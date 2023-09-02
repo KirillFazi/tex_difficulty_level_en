f6d9efee1ea347a45ed6f1a093a3806607d11b36
KirillFazi <izyablok@mail.ru>
2023-09-02 15:45:41 +0300

    PC

--- test_get_request.py
+++ test_get_request.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     print('Send request to the server')
     print('Please, wait a few seconds...')
 
-    df = pd.read_csv("../data/English_scores/movie_subs_level.csv")
+    df = pd.read_csv("data/movie_subs_level.csv")
     subs = df['subs'][22]
 
     response_data = make_get_request(subs)
